ef21c77acd92e7359b65c997dc5a9ea5863363c6
Martijn Millecamp <martijn.millecamp@ae.be>
2022-01-16 11:29:17 +0100

    trying to fix error static files

--- spweadmin/settings.py
+++ spweadmin/settings.py
@@ -18,13 +18,13 @@ import django_heroku
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 
